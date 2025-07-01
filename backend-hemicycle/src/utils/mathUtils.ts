9247b136f16b805077b0570cfa68d1b3a26d5a58
Pareilart <pareilart@gmail.com>
2025-07-01 15:01:41 +0200

    BACK: refactor: Simplification des fonctions mathématiques en utilisant des expressions fléchées pour améliorer la lisibilité du code.

--- backend-hemicycle/src/utils/mathUtils.ts
+++ backend-hemicycle/src/utils/mathUtils.ts
@@ -2,14 +2,8 @@
  * Just an example of a unit test file
  */
 
-export const add = (a: number, b: number): number => {
-  return a + b;
-};
+export const add = (a: number, b: number): number => a + b;
 
-export const multiply = (a: number, b: number): number => {
-  return a * b;
-};
+export const multiply = (a: number, b: number): number => a * b;
 
-export const subtract = (a: number, b: number): number => {
-  return a - b;
-};
+export const subtract = (a: number, b: number): number => a - b;
