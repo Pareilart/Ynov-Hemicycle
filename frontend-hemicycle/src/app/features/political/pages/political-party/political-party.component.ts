2d50597f6413603c8247efca11d01ee58a6634ef
Julie <julie.albini@edu.univ-fcomte.fr>
2025-07-03 11:57:54 +0200

    [TASK] Partis politiques (2)

--- frontend-hemicycle/src/app/features/political/pages/political-party/political-party.component.ts
+++ frontend-hemicycle/src/app/features/political/pages/political-party/political-party.component.ts
@@ -19,17 +19,17 @@ import { ProgressBarModule } from 'primeng/progressbar';
 })
 export class PoliticalPartyComponent {
   public partis = [
-    { name:"Rassemblement National", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social' },
-    { name:"Ensemble pour la République", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"La France insoumise", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"Socialiste et apparentés", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"Droite Républicaine", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"Ecologiste et Social", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"Les démocrates", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"Horizons et indépendants", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"Libertés, indépendants, Outre-mer et Territoires", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"Gauche Démocrate et Républicaine", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"UDR", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
-    { name:"Députés non inscrits", src: 'images/ecolo.jpg', alt: 'Logo parti Ecologiste et social'},
+    { name:"Rassemblement National", src: 'images/rn.png', alt: 'Logo parti Rassemblement National', nb:'123' },
+    { name:"Ensemble pour la République", src: 'images/ensemble.png', alt: 'Logo parti Ensemble pour la République', nb:'93'},
+    { name:"La France insoumise", src: 'images/france_insoumise.svg', alt: 'Logo parti La France insoumise', nb:'71'},
+    { name:"Socialiste et apparentés", src: 'images/socialiste.png', alt: 'Logo parti Socialiste et apparentés', nb:'66'},
+    { name:"Droite Républicaine", src: 'images/droite_republicaine.png', alt: 'Logo parti Droite Républicaine', nb:'49'},
+    { name:"Ecologiste et Social", src: 'images/ecologiste_social.png', alt: 'Logo parti Ecologiste et social', nb:'38'},
+    { name:"Les démocrates", src: 'images/democrates.png', alt: 'Logo parti Les démocrates', nb:'36'},
+    { name:"Horizons et indépendants", src: 'images/horizons.png', alt: 'Logo parti Horizons et indépendants', nb:'34'},
+    { name:"Libertés, indépendants, Outre-mer et Territoires", src: 'images/liot.png', alt: 'Logo parti Libertés, indépendants, Outre-mer et Territoires', nb:'23'},
+    { name:"Gauche Démocrate et Républicaine", src: 'images/gdr.png', alt: 'Logo parti Gauche Démocrate et Républicaine', nb:'17'},
+    { name:"UDR", src: 'images/udr.png', alt: 'Logo parti UDR', nb:'16'},
+    { name:"Députés non inscrits", src: 'images/ni.png', alt: 'Logo parti Députés non inscrits', nb:'11'},
   ];
 }
