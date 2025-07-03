f2de733380cfa0a8429274d6d7be80c00f380a16
Julie <julie.albini@edu.univ-fcomte.fr>
2025-07-03 14:25:58 +0200

    [TASK] Popin profil

--- frontend-hemicycle/src/app/layouts/main-layout/partials/main-layout-header/main-layout-header.component.ts
+++ frontend-hemicycle/src/app/layouts/main-layout/partials/main-layout-header/main-layout-header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { afterRenderEffect, Component, computed, DestroyRef, effect, EffectCleanupRegisterFn, ElementRef, inject, input, InputSignal, PLATFORM_ID, Signal, signal, viewChild, WritableSignal } from '@angular/core';
+import { afterRenderEffect, Component, computed, DestroyRef, effect, EffectCleanupRegisterFn, ElementRef, inject, input, InputSignal, PLATFORM_ID, Signal, signal, viewChild, WritableSignal, ViewChild  } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { environment } from '@environments/environment';
 import { MegaMenuItem, PrimeIcons } from 'primeng/api';
@@ -8,6 +8,12 @@ import { ScrollDispatcher, ViewportRuler, ViewportScrollPosition } from '@angula
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Subscription } from 'rxjs';
 import { Title } from '@angular/platform-browser';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-main-layout-header',
@@ -15,11 +21,37 @@ import { Title } from '@angular/platform-browser';
     CommonModule,
     RouterModule,
     MegaMenuModule,
+    MenuModule,
+    BadgeModule,
+    AvatarModule,
+    ButtonModule  
   ],
   templateUrl: './main-layout-header.component.html',
   styleUrl: './main-layout-header.component.css',
 })
 export class MainLayoutHeaderComponent {
+
+  @ViewChild('profileMenuContainer', { static: true }) profileMenuContainer!: ElementRef;
+  constructor(private router: Router) {}
+
+  profileMenuItems = [
+    { label: 'Mon profil', icon: 'pi pi-user', command: () => this.goToProfile() },
+    { label: 'Paramètres', icon: 'pi pi-cog', command: () => this.goToSettings() },
+    { separator: true },
+    { label: 'Déconnexion', icon: 'pi pi-sign-out', }
+  ];
+
+  goToProfile() {
+    // Navigation vers le profil
+    this.router.navigate(['/profil/user']);
+  }
+
+  goToSettings() {
+    // Navigation vers paramètres
+    this.router.navigate(['/settings']);
+  }
+
+
   //#region Propriétés
   /**
    * Propriété appName
